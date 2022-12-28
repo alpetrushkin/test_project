d48f89bb41624b08da6b79a81b8dd4eb5a4378a6
alpetrushkin <alpetrushkin1984@mail.ru>
2022-12-28 19:55:08 +0300

    события в React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import {Component} from "react";
+
+class WhoAmI extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         years: 27,
+         position: ''
+      }
+   }
+
+   nextYear = () => {
+      this.setState(state => ({
+         years: state.years + 1
+      }))
+   }
+
+   commitInputChanges = (e) => {
+      this.setState({
+         position: e.currentTarget.value
+      })
+   }
+
+   render() {
+      const {name, surname, link} = this.props;
+      const {position, years} = this.state;
+      return (
+         <div>
+            <button onClick={this.nextYear}>+++</button>
+            <h1>My name is {name}, surname - {surname}, age - {years}, position - {position}</h1>
+            <a href={link}>My profile</a>
+            <form>
+               <span>Введите должность</span>
+               <input type="text" onChange={this.commitInputChanges}/>
+            </form>
+         </div>
+      )
+
+   }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <div className="App">
+         <WhoAmI/>
+      </div>
+   )
+
 }
 
 export default App;
